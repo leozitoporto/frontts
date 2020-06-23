6432ba68f4109f7387c80225b299b82a3dc54683
Leonardo <leonardo@zungasoft.net>
2020-06-23 20:18:21 -0300

    Criando o Header

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,18 +1,24 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  avatar_url: string;
+  name: string;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
+
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
-interface AuthState {
-  token: string;
-  user: object;
-}
-
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
